32a91c20ef550f2009de518ac373effa7c1a7a36
yfktn <friskantoni@gmail.com>
2022-03-23 06:16:03 +0700

    Sekarang lakukan crawler sampai ke dialog untuk penyedia

--- rup-penyedia.js
+++ rup-penyedia.js
@@ -9,7 +9,13 @@ if(!url) {
 }
 
 async function run(halamanUtama) {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({
+        headless: true, 
+        timeout: 10000,
+        args: [
+            '--shm-size=3gb','--no-sandbox', '--disable-setuid-sandbox'
+        ]
+    });
     const page = await browser.newPage();
 
     page.on('console', (msg) => console.log('PAGE_LOG:', msg.text()));
@@ -21,8 +27,8 @@ async function run(halamanUtama) {
 
     let keepCrawling = true, 
         currentPage = 1,
-        maxPage = 2, // -1 for unlimited
-        delay = 1500,
+        maxPage = -1, // -1 for unlimited
+        delay = 1000,
         randomStr = (Math.random() + 1).toString(36).substring(7),
         currentFileName = 'sirup-penyedia-' + randomStr,
         data = [];
@@ -39,30 +45,35 @@ async function run(halamanUtama) {
             keepCrawling = false;
             break; // keluar!
         }
-        // const data = await page.evaluate(() => {
-        //     const rupTable = Array.from(document.querySelectorAll('table#tblLelangLama tbody tr'));
-        //     return rupTable.map((row) => {
-        //         const columns = Array.from(row.querySelectorAll('td'));
-
-        //         return {
-        //             satker: columns[1].innerText,
-        //             nama_paket: columns[2].innerText,
-        //             pagu: columns[3].innerText.replace(/[^0-9,]/g, ''),
-        //             metode: columns[4].innerText,
-        //             sumber_dana: columns[5].innerText.trim(),
-        //             kode_rup: columns[6].innerText.trim(),
-        //             waktu_pemilihan: columns[7].innerText.trim()
-        //         };
-
-        //     });
-        // });
-        
         // get link to opening dialog windows, click to open and read the content
-        const links = page.$$('table#tblLelangLama tbody tr td:nth-child(3) a');
+        const links = await page.$$('table#tblLelangLama tbody tr td:nth-child(3) a'),
+            linksLength = links.length;
+        let isError = false;
         data = []; // init first
-        for(const link in links) {
-            await link.click();
-            await waitTillHTMLRendered.waitTillHTMLRendered(page, { querySelector: "div#myModal" });
+        for(linkOffset = 0; linkOffset < linksLength; linkOffset++) {
+            // /sirup/home/detailPaketPenyediaPublic2017/31150077
+            // we need to get RUP ID
+            const href = await page.evaluate(link => link.href, links[linkOffset]);
+            const hrefSplit = href.split("/"),
+                idRup = hrefSplit[hrefSplit.length - 1]; 
+
+            await page.evaluate(linkOffset => {
+                    const linkToClickSelector = `table#tblLelangLama tbody tr:nth-child(${linkOffset+1}) td:nth-child(3) a:nth-child(1)`;
+                    // console.log('Click yo', linkToClickSelector);
+                    document.querySelector(linkToClickSelector).click();
+                }, linkOffset)
+                .then(()=>console.log('Data #', linkOffset, ' page ', currentPage, ' load modal of :', href));
+            await page.waitForNetworkIdle()
+                .then(() => console.log('Loaded'));
+            try {
+                await page.waitForSelector('div#detil > table tbody tr:nth-child(1) td:nth-child(2)'); // wait for the element of RUP ID
+                await page.waitForFunction("document.querySelector('div#detil > table tbody tr:nth-child(1) td:nth-child(2)').innerText == '" + idRup + "'");
+            } catch(error) {
+                console.log("Tidak dapat melakukan pengambilan load baru!", error);
+                isError = true;
+                break;
+            }
+            // await waitTillHTMLRendered.waitTillHTMLRendered(page, { querySelector: "div#myModal" });
             const retd = await page.evaluate(() => {
                 // Only chose the main table of div#detil and ignore another table inside it!
                 const detailTableRows = Array.from(document.querySelectorAll('div#detil > table > tbody > tr'));
@@ -79,6 +90,7 @@ async function run(halamanUtama) {
                         case 'spesifikasi pekerjaan': dataInPage['spesifikasi_pekerjaan'] = row.querySelector('td:nth-child(2)').innerText.trim(); break;
                         case 'produk dalam negeri': dataInPage['produk_dalam_negeri'] = row.querySelector('td:nth-child(2)').innerText.trim(); break;
                         case 'usaha kecil/koperasi': dataInPage['usaha_kecil_koperasi'] = row.querySelector('td:nth-child(2)').innerText.trim(); break;
+                        case 'alasan bukan usaha kecil/koperasi': dataInPage['alasan_bukan_usaha_kecil_koperasi'] = row.querySelector('td:nth-child(2)').innerText.trim(); break;
                         case 'pra dipa / dpa': dataInPage['pra_dipa_dpa'] = row.querySelector('td:nth-child(2)').innerText.trim(); break;
                         case 'jenis pengadaan': dataInPage['jenis_pengadaan'] = row.querySelector('td:nth-child(2)').innerText.trim(); break;
                         case 'jenis pengadaan': dataInPage['jenis_pengadaan'] = row.querySelector('td:nth-child(2)').innerText.trim(); break;
@@ -100,8 +112,36 @@ async function run(halamanUtama) {
                             dataInPage['pemilihan_mulai'] = tableDt[0].innerText;
                             dataInPage['pemilihan_akhir'] = tableDt[1].innerText;
                             break;
+                        case 'lokasi pekerjaan':
+                            tableDt = Array.from(row.querySelectorAll('td:nth-child(2) table tbody tr'));
+                            dataInPage['lokasi'] = tableDt.map(row => {
+                                if(row.querySelector('td:nth-child(2)') == null) {
+                                    return false;
+                                }
+                                return [
+                                    row.querySelector('td:nth-child(2)').innerText,
+                                    row.querySelector('td:nth-child(3)').innerText,
+                                    row.querySelector('td:nth-child(4)').innerText,
+                                ].join('>');
+                            }).filter(i => i != false).join('|');
+                            break;
+                        case 'sumber dana':
+                            tableDt = Array.from(row.querySelectorAll('td:nth-child(2) table tbody tr'));
+                            dataInPage['sumber_dana'] = tableDt.map(row => {
+                                if(row.querySelector('td:nth-child(2)') == null) {
+                                    return false;
+                                }
+                                return [
+                                    row.querySelector('td:nth-child(2)').innerText,
+                                    row.querySelector('td:nth-child(3)').innerText,
+                                    row.querySelector('td:nth-child(4)').innerText,
+                                    row.querySelector('td:nth-child(5)').innerText,
+                                    row.querySelector('td:nth-child(6)').innerText,
+                                ].join('|');
+                            }).filter(i => i != false).join(',');
+                            break;
                     }
-                })
+                });
                 return dataInPage;
                 // return {
                 //     kode_rup: detailTableRows[0].querySelector('td:nth-child(2)').innerText,
@@ -118,12 +158,23 @@ async function run(halamanUtama) {
                 // }
             });
             data.push(retd);
+            // tutup modal
+            await page.waitForSelector('div#myModal .modal-footer button.btn.btn-rup.btn-redPastel');
+            await page.evaluate(() => document.querySelector('div#myModal .modal-footer button.btn.btn-rup.btn-redPastel').click());
+            await page.waitForTimeout(350);
+            await page.waitForSelector('div#myModal', {
+                visible: false
+            });
         }
 
 
         // save our data
         const pageStr = ('' + currentPage).padStart(4, '0');
         await fs.writeFile(currentFileName + '-' + pageStr + '.db', JSON.stringify(data));
+        if(isError) {
+            console.log("FORCE BREAK we got some error!");
+            break;
+        }
         // next button "Selanjutnya" exist if it doesn't in disabled state!
         const isNextButtonExist = await page.evaluate(() => document.querySelector('li#tblLelangLama_next.disabled') == null);
         if(!isNextButtonExist) {
@@ -132,7 +183,7 @@ async function run(halamanUtama) {
             keepCrawling = false;
         } else {
             currentPage++;
-            page.waitForTimeout(delay);
+            await page.waitForTimeout(delay);
             await waiterAndTool.clickNextPage(page);
         }
     }
